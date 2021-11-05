a6d1aa9b9c4ba320e64d6885ff62f480a6fe0b19
Kmsz <kkamasooz@gmail.com>
2021-11-04 22:44:32 -0300

    Create interface.py

---
+++ interface.py
@@ -0,0 +1,59 @@
+# Login interface with Tkinter
+#       Functions
+from tkinter import * 
+from tkinter import messagebox
+
+def mensagem(name, texto):
+  messagebox.showinfo(name, texto)
+  quit()
+
+#  Window settings.
+def create_window():
+  window = Tk()
+  window.title("Kmsz Login")
+
+  window['bg'] = "#1f211f"
+  window.geometry("210x200+290+290")
+
+  window.resizable(False, False)
+  window.iconbitmap("icon.ico")
+
+  #Cascades
+  menu = Menu(window)
+  window.config(menu=menu)
+  filemenu = Menu(menu)
+  
+  menu.add_cascade(label = "Help",
+    menu = filemenu)
+
+  filemenu.add_command(label='Exit',
+    command = window.quit)
+
+  # Labels
+  label_user = Label(window,
+    text = "User:",
+    font = "Arial 12 italic",
+    fg = "white",
+    bg = "#1f211f"
+  ).grid( row = 0, column = 0, sticky = W )
+
+  label_password = Label(window,
+    text = "Password:",
+    font = "Arial 12 italic",
+    fg = "white",
+    bg = "#1f211f"
+  ).grid( row = 1, column = 0, sticky = W )
+
+  #Entrys 
+  entry_user = Entry(window).grid( row = 0, column = 1)
+  entry_password = Entry(window).grid ( row = 1, column = 1)
+
+  #Buttons
+  button_login = Button(window,
+    text = "Login",
+    font = "Arial 12 italic",
+    command = lambda: mensagem("Login Access", "Successfully logged in")
+    ).grid(row = 2, column = 1, sticky = E)
+
+  # Loop window
+  window.mainloop()
